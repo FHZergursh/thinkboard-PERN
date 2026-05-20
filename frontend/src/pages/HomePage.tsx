976e4088ae44c1ad727aac4998e8d0c74f6b2cf6
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-05-20 20:17:13 +0100

    add a card for each note and display on home page
    title

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
-import axios from 'axios'
+import NoteCardFixed from '../components/NoteCardFixed'
+
+interface noteInterface {
+  id: number,
+  title: string,
+  content: string
+}
 
 
 const HomePage = () => {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState<noteInterface[]>([])
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes")
-        console.log(res.data)
-        setNotes(res.data)
+        const res = await fetch("http://localhost:5001/api/notes")
+        const result = await res.json();
+        setNotes(result.data)
 
       } catch (error) {
-        console.log("Error fetching notes")
+        console.log("Error fetching notes, ", error)
 
       } finally {
         setLoading(false)
@@ -29,7 +35,22 @@ const HomePage = () => {
     <div className='min-h-screen'>
       <Navbar />
 
-      <div className=''></div>
+      <div className='max-w-7xl mx-auto p-4 mt-6'>
+        {loading && <div className='text-center text-primary py-10'>loading notes... </div>}
+
+        {notes.length > 0 && (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {notes.map(note => (
+              <NoteCardFixed key={note.id} note={note} />
+            ))}
+            
+          </div>
+        )}
+
+
+
+
+      </div>
 
       
 

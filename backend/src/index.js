b510ee25eee6852104573e035527b12e66c26edd
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-05-20 19:11:31 +0100

    fix cors error, begin work on homepage
    title

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import router from "./routes/notesRoutes.js";
 import { setupDB } from "./db/db.js";
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/notes', router);
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
+import axios from 'axios'
+
 
 const HomePage = () => {
+  const [notes, setNotes] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes")
+        console.log(res.data)
+        setNotes(res.data)
+
+      } catch (error) {
+        console.log("Error fetching notes")
+
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchNotes()
+  }, [])
+
+
   return (
     <div className='min-h-screen'>
       <Navbar />
+
+      <div className=''></div>
 
       
 
